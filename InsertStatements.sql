b65bb79fac390027ca518fb1bcd2a29e3341b6fb
ElyseB <51666554+ElyseB@users.noreply.github.com>
2019-10-27 06:57:00 -0500

    Add files via upload

---
+++ InsertStatements.sql
@@ -0,0 +1,12 @@
+INSERT ALL
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Abbottsford Hall', 'AB', '722 N 13th St', '414-288-5468')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Carpenter Tower', 'CT', '1032 W. Wisconsin Ave', '414-288-7606')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Cobeen Hall', 'CO', '1111 W. Wells St', '414-288-3131')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Humphrey Hall', 'HU', '1716 W. Wisconsin Ave', '414-288-7616')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Mashuda', 'MA', '1926 W. Wisconsin Ave', '414-288-3100')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('ODonnell', 'OD', '725 N. 18th St', '414-288-7800')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Schroeder Hall', 'WS', '725 N. 13th St', '414-288-6464')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Straz Tower', 'ST', '915 W. Wisconsin Ave', '414-288-3579')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Eckstein Tower', 'ET', '721 N. 17th St', '414-288-1725')
+    INTO BUILDING (BName, BAbbrev, BAddress, BPhone) VALUES ('Wells Hall', 'WH', '721 N. 17th St', '414-288-0792')
+SELECT * FROM dual;
