552d0d6d2986e546e6c325a8a46389ad70667f3b
ElyseB <51666554+ElyseB@users.noreply.github.com>
2019-11-13 06:22:03 -0600

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE RESERVATION(
     companyName VARCHAR2(250), 
     contactName VARCHAR2 (250) NOT NULL,
     contactPhone VARCHAR2 (15) NOT NULL,
-    occupies INT,
+    occupies VARCHAR2(5),
     reservStart DATE,
     reservEnd DATE,
     roomsNeeded INT,
