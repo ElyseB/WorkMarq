a10e0267835571d364c6fa80d15dc8ecf0b0fc63
ElyseB <51666554+ElyseB@users.noreply.github.com>
2019-11-15 06:01:42 -0600

    New File

---
+++ ReservationMapper.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface ReservationMapper {
+	@Select("SELECT reservNum, orgName, contactName, contactPhone, occupies, reservStart, reservEnd, roomsNeeded FROM RESERVATION)
+	List<Reservation> getReservationList();
+	
+	@Select("SELECT reservNum, orgName, contactName, contactPhone, occupies, reservStart, reservEnd, roomsNeeded FROM RESERVATION where reservNum = #{reservNum}")
+	Reservation getReservation(Reservation reservation);
+	
+	@Update("update reservation set reservNum = #{reservNum}, orgName = #{orgName}, contactName = #{contactName}, contactPhone = #{contactPhone}, occupies = #{occupies}, reservStart = #{reservStart}, reservEnd = #{reservEnd} where reservNum = #{reservNum}")
+	void updateReservation(Reservation reservation);
+	
+	@Insert("insert into reservation(reservNum, orgName, contactName, contactPhone, occupies, reservStart, reservEnd, roomsNeeded) values (#{reservNum}, #{orgName}, #{contactName}, #{contactPhone}, #{occupies}, #{reservStart}, #{reservEnd}, #{roomsNeeded})")
+	void insertReservation(Reservation reservation);
+}
