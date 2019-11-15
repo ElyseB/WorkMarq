77ae799c163478a1d6e1c208189bbf183a11109e
nikkigabbard <32397778+nikkigabbard@users.noreply.github.com>
2019-11-15 03:55:02 -0600

    Update BuildingService.java

--- BuildingService.java
+++ BuildingService.java
@@ -1,5 +1,37 @@
 package com.example.demo.service;
 
-public class BuildingService {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.dao.BuildingDAO;
+import com.example.demo.domain.Building;
+
+@Component
+public class BuildingService {
+	
+	@Autowired
+	BuildingDAO buildingDAO;
+	
+	public BuildingService() {
+		
+	}
+	public List<Building> getBuildingList(){
+		List<Building> buildingList = new ArrayList<Building>();
+		buildingList = buildingDAO.getBuildingList();
+		return buildingList;
+	}
+	public void insertBuilding(Building building) {
+		buildingDAO.insertBuilding(building);
+	}
+	public Building getBuilding(Building building) {
+		return buildingDAO.getBuilding(building);
+	}
+	public void updateBuilding(Building building) {
+		buildingDAO.updateBuilding(building);
+	}
 }
