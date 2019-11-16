9f794d59e03165d510cfe5ec8688058c00eb7d42
nikkigabbard <32397778+nikkigabbard@users.noreply.github.com>
2019-11-16 03:35:06 -0600

    Create WorkMarqApplication.java

---
+++ WorkMarq/src/main/java/com.Marq.WorkMarq/WorkMarqApplication.java
@@ -0,0 +1,13 @@
+package com.Marq.WorkMarq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMarqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMarqApplication.class, args);
+	}
+
+}
