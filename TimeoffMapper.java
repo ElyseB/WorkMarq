a796bd51d38f41f379db52c7eb3eb848110e1078
ElyseB <51666554+ElyseB@users.noreply.github.com>
2019-11-20 07:40:43 -0600

    Updated table columns

--- TimeoffMapper.java
+++ TimeoffMapper.java
@@ -9,15 +9,15 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface TimeoffMapper {
-	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed FROM TIMEOFF)
+	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed, submitted FROM TIMEOFF)
 	List<Timeoff> getTimeoffList();
 	
-	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed FROM TIMEOFF where empNum = #{empNum}")
+	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed, submitted FROM TIMEOFF where empNum = #{empNum}")
 	Timeoff getTimeoff(Timeoff timeoff);
 	
-	@Update("update timeoff set empNum = #{empNum}, startDate = #{startDate}, endDate = #{endDate}, totalHours = #{totalHours}, typeHoursUsed = #{typeHoursUsed} where empNum = #{empNum}")
+	@Update("update timeoff set empNum = #{empNum}, startDate = #{startDate}, endDate = #{endDate}, totalHours = #{totalHours}, typeHoursUsed = #{typeHoursUsed}, submitted = #{submitted} where empNum = #{empNum}")
 	void updateTimeoff(Timeoff timeoff);
 	
-	@Insert("insert into timeoff(empNum, startDate, endDate, totalHours, typeHoursUsed) values (#{empNum}, #{startDate}, #{endDate}, #{totalHours}, #{typeHoursUsed})")
+	@Insert("insert into timeoff(empNum, startDate, endDate, totalHours, typeHoursUsed, submitted) values (#{empNum}, #{startDate}, #{endDate}, #{totalHours}, #{typeHoursUsed}, #{submitted})")
 	void insertTimeoff(Timeoff timeoff);
 }
