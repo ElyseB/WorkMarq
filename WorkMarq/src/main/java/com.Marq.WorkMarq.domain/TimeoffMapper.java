a846031cbd6696e9fe16a36c62c7dc55695bcc09
nikkigabbard <32397778+nikkigabbard@users.noreply.github.com>
2019-11-25 02:39:36 -0600

    Create TimeoffMapper.java

---
+++ WorkMarq/src/main/java/com.Marq.WorkMarq.domain/TimeoffMapper.java
@@ -0,0 +1,29 @@
+package com.Marq.WorkMarq.domain;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface TimeoffMapper {
+	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed FROM TIMEOFF")
+	List<Timeoff> getTimeoffList();
+	
+	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed FROM TIMEOFF where empNum = #{empNum}")
+	List<Timeoff> getMyTimeoffList();
+	
+	@Select("SELECT empNum, startDate, endDate, totalHours, typeHoursUsed FROM TIMEOFF where empNum = #{empNum}")
+	Timeoff getTimeoff(Timeoff timeoff);
+	
+	@Select("SELECT * FROM TIMEOFF where cardNum = #{cardNum}")
+	Timeoff getMyTimeoff(Timeoff timeoff);
+	
+	@Update("update timeoff set empNum = #{empNum}, startDate = #{startDate}, endDate = #{endDate}, totalHours = #{totalHours}, typeHoursUsed = #{typeHoursUsed} where empNum = #{empNum}")
+	void updateTimeoff(Timeoff timeoff);
+	
+	@Insert("insert into timeoff(empNum, startDate, endDate, totalHours, typeHoursUsed) values (#{empNum}, #{startDate}, #{endDate}, #{totalHours}, #{typeHoursUsed})")
+	void insertTimeoff(Timeoff timeoff);
+}
